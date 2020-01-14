41ce69d4623f44c16d0022a81d8086aad8702cc3
Yan Uehara <yanuehara@hotmail.com>
2020-01-14 19:45:52 -0400

    Project structure correction and routes addition
    Corrected project structure		index.js => src/index.js
    Segreggated routes in separate file	src/routes.js
    
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
@@ -9,5 +10,6 @@ mongoose.connect('mongodb://localhost:27017/omnistack', {
 });
 
 app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
