9a59d2fcd292700cf4db9c89cf2b53e25c356efb
Yan Uehara <yanuehara@hotmail.com>
2020-01-14 19:39:37 -0400

    Adding mongoose and connection string
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/omnistack', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
 
 app.listen(3333);
