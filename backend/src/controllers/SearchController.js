cbd5a459ea56ba58c57afb7f4fe72426eebb19d0
Yan Uehara <yanuehara@hotmail.com>
2020-01-14 22:35:56 -0400

    Adding search route
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,29 @@
+const Dev = require('../models/Dev');
+const parseStringToArray = require('../utils/parseStringToArray');
+
+module.exports = {
+    async index(req, res) {
+        const {latitude, longitude, techs} = req.query;
+
+        const techsArray = parseStringToArray(techs);
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude]
+                    },
+                    $maxDistance: 10000 //10km
+                }
+            }
+        });
+
+        return res.status(200).json({
+            devs
+        });
+    }
+}
