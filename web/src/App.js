cb0fd38b8c492098fad7e4b226b2e05d94611391
Yan Uehara <yanuehara@hotmail.com>
2020-01-16 18:40:43 -0400

    Adding main content style and placeholder code
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,7 @@ import './global.css';
 import './App.css';
 
 import './Sidebar.css';
+import './Main.css';
 
 function App() {
   return (
@@ -12,21 +13,21 @@ function App() {
         <strong>Cadastrar</strong>
 
         <form>
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
             <input name="github_username" id="github_username" required/>
           </div>
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
             <input name="techs" id="techs" required/>
           </div>
 
-          <div class="input-group">
-            <div class="input-block">
+          <div className="input-group">
+            <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
               <input name="latitude" id="latitude" required/>
             </div>
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
               <input name="longitude" id="longitude" required/>
             </div>
@@ -36,7 +37,55 @@ function App() {
         </form>
       </aside>
       <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/9919?s=200&v=4" alt="Name"/>
+              <div className="user-info">
+                <strong>name</strong>
+                <span>techs</span>
+                <p>Bio.</p>
+                <a href="github.com/yanuehara">Acessar perfil no github</a>
+              </div>
+            </header>
+          </li>
 
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/9919?s=200&v=4" alt="Yan Uehara"/>
+              <div className="user-info">
+                <strong>name</strong>
+                <span>techs</span>
+                <p>Bio.</p>
+                <a href="github.com/yanuehara">Acessar perfil no github</a>
+              </div>
+            </header>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/9919?s=200&v=4" alt="Yan Uehara"/>
+              <div className="user-info">
+                <strong>name</strong>
+                <span>techs</span>
+                <p>Bio.</p>
+                <a href="github.com/yanuehara">Acessar perfil no github</a>
+              </div>
+            </header>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/9919?s=200&v=4" alt="Yan Uehara"/>
+              <div className="user-info">
+                <strong>name</strong>
+                <span>techs</span>
+                <p>Bio.</p>
+                <a href="github.com/yanuehara">Acessar perfil no github</a>
+              </div>
+            </header>
+          </li>
+        </ul>
       </main>
     </div>
   );
