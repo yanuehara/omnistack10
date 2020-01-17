b2a2eb2da9559e2c79588c9abb99448447345031
Yan Uehara <yanuehara@hotmail.com>
2020-01-17 11:38:13 -0400

    Adding view placeholders
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,140 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity} from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
-function Main(){
-    return <View />
+function Main(props){
+    const [currentRegion, setCurrentRegion] = useState(null);
+    const { navigation } = props;
+
+    useEffect( () => {
+        async function loadInitialPosition(){
+            const { granted } = await requestPermissionsAsync();
+            
+            if (granted) {
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true
+                });
+
+                const { latitude, longitude } = coords;
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04
+                });
+            }
+        }
+
+        loadInitialPosition();
+    }, []);
+
+    if(!currentRegion){
+        return null;
+    }
+
+    return (
+        <>
+            <MapView initialRegion={ currentRegion } style={ styles.map }>
+                <Marker coordinate={{ latitude: -19.0013553, longitude: -57.629266 }}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/9919?s=200&v=4' }}/>
+
+                    <Callout onPress={ () => {
+                        navigation.navigate('Profile', {github_username: 'github'});
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Nome</Text>
+                            <Text style={styles.devBio}>Bio.</Text>
+                            <Text style={styles.devTechs}>Techs</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+
+            <View style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por tecnologias"
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity style={styles.loadButton} onPress={ () => {}}>
+                    <MaterialIcons name="my-location" size={20} color="#FFF"/>
+                </TouchableOpacity>
+            </View>
+        </>
+    );
 }
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    },
+
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#FFF'
+    },
+
+    callout: {
+        width: 260
+    },
+
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+
+    devBio: {
+        color: '#666',
+        marginTop: 5
+    },
+
+    devTechs: {
+        marginTop: 5
+    },
+
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row'
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4
+        },
+        elevation: 2
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8d4eff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15
+    }
+});
 
 export default Main;
